f6ebf5a20a71c33d43409ed7b68cb48f4fbcc361
Janon Emersion Thanendren <janonemersion@hotmail.com>
2024-08-07 12:34:21 +0530

    v1.1.0

--- lkp/lkp/urls.py
+++ lkp/lkp/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('web.urls_web')),
+    path('', include('support.urls_support')),
+    path('', include('landing.urls_landing')),
+    path('', include('faq.urls_faq')),
+    path('', include('blog.urls_blog')),
     path('', include('app.urls_app')),
     path('admin/', admin.site.urls),
 ]
--- lkp/support/views.py
+++ lkp/support/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def privacy(request):
+    return render (request, 'support/privacy.html')
+
+def terms_of_service(request):
+    return render (request, 'support/terms_of_service.html')
+
+def cookie_policy(request):
+    return render (request, 'support/cookie_policy.html')
+
+def right_of_withdrawal(request):
+    return render (request, 'support/right_of_withdrawal.html')
