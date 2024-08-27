88cbb1fdd766553a5933ddb8b222c49a4070112a
Janon Emersion Thanendren <janonemersion@hotmail.com>
2024-08-27 19:32:44 +0530

    00.00.001

--- lkprofessionals/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . views import *
-
-urlpatterns = [
-    path('blog/', blog, name='blog'),
-    path('blog-single/', blog_single, name='blog-single'),
-]
-
--- lkprofessionals/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def blog(request):
-    return render(request, 'web/blog.html')
-
-def blog_single(request):
-    return render(request, 'web/blog-single.html')
